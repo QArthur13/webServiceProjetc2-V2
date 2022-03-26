b561ea51644d22daad29f7b955ad5f09e03822e7
Quaranta Arthur <arthur.quaranta@ynov.com>
2022-03-26 15:10:51 +0100

    "Add default"

---
+++ src/Controller/ApiDefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ApiDefaultController extends AbstractController
+{
+    #[Route('/api/default', name: 'app_api_default', methods: ["GET"])]
+    public function index(Request $request, SerializerInterface $serializer): Response
+    {
+        if ('application/xml' === $request->headers->get('Accept')) {
+
+            $formats = 'xlm';
+            $contentType = 'application/xml';
+
+        } else {
+
+            $formats = 'json';
+            $contentType = 'application/json';
+
+        }
+
+        return  new  Response(
+
+            $serializer->serialize(['message' => 'Test'], $formats),
+            Response::HTTP_OK,
+            ['Content-Type' => $contentType]
+
+        );
+    }
+}
